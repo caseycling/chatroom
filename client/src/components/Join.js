16800467cd34ca04192ce83a0777b173d81efa2c
Casey <caseyclinga@gmail.com>
2020-05-27 13:43:22 -0700

    Initial commit with MVC structure

---
+++ client/src/components/Join.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Join = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  return <div>Join</div>;
+};
+
+export default Join;
