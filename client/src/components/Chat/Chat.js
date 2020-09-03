7600240c63ea52a757ec25dfab85cd4c3e8d5948
Casey <caseyclinga@gmail.com>
2020-09-02 19:43:20 -0700

    integrated socket.IO server into chat component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io, { Socket } from 'socket.io-client';
 
-function Chat() {
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:5000';
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit('join', { name, room }, () => {
+      //
+    });
+
+    return () => {
+      socket.emit('disconnect');
+
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+
   return <h1>Chat</h1>;
-}
+};
 
 export default Chat;
