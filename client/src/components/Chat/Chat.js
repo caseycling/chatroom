bc10bfe6ac8d20b638ec9adc22b53320d28297cb
Casey <caseyclinga@gmail.com>
2020-09-06 13:30:25 -0700

    Rearranged folder structure

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
-import io, { Socket } from 'socket.io-client';
+import io from 'socket.io-client';
 
 import InfoBar from '../InfoBar/InfoBar';
 import Messages from '../Messages/Messages';
